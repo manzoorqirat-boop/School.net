0c4fdbaed37347741fbe41399bc7f0928cf94964
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-07-24 09:28:06 +0000

    Fix: Mode column type mismatch causing migration crash

--- Migrations/20260724092758_FixAttendanceModeColumnType.Designer.cs
+++ Migrations/20260724092758_FixAttendanceModeColumnType.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QMSoft.Api.Data;
@@ -13,9 +14,11 @@ using QMSoft.Api.Data;
 namespace QMSoft.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260724092758_FixAttendanceModeColumnType")]
+    partial class FixAttendanceModeColumnType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
